bcc59ee6382fb05ef5bdeeb4755773c083d01f94
nikkantei <10057793@network.rca.ac.uk>
2025-07-10 21:36:46 +0100

    Update generateImage.js

--- api/generateImage.js
+++ api/generateImage.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.openai.com/v1/images/generations", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Authorization": `Bearer ${process.env.VITE_OPENAI_API_KEY}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
