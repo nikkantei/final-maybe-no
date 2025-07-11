45579479d4bae0723557dd9e6ad861c4c71eb3d2
nikkantei <10057793@network.rca.ac.uk>
2025-07-11 17:02:05 +0100

    Update pdfExport.js

--- src/utils/pdfExport.js
+++ src/utils/pdfExport.js
@@ -2,7 +2,20 @@ import jsPDF from 'jspdf';
 
 export function downloadAsPDF(content, filename = "CivicHorizon_Vision.pdf") {
   const doc = new jsPDF();
-  const lines = doc.splitTextToSize(content, 180); // 180 = width
-  doc.text(lines, 15, 20);
+  const margin = 15;
+  const lineHeight = 10;
+  const maxLineWidth = 180;
+  const lines = doc.splitTextToSize(content, maxLineWidth);
+  let cursorY = 20;
+
+  lines.forEach((line) => {
+    if (cursorY + lineHeight > doc.internal.pageSize.height - margin) {
+      doc.addPage();
+      cursorY = margin;
+    }
+    doc.text(line, margin, cursorY);
+    cursorY += lineHeight;
+  });
+
   doc.save(filename);
 }
